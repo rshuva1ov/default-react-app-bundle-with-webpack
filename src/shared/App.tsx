886f05fbad8b74ee38d83b20420900138a0139a4
unknown <shuvalov.rem@mail.ru>
2023-10-02 14:43:28 +0800

    add bundle

---
+++ src/shared/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppComponent } from './Components/AppComponent';
+import './main.global.css';
+import { hot } from 'react-hot-loader/root';
+
+export const App = hot(() => (
+  <AppComponent />
+));
---
+++ src/shared/Components/AppComponent/AppComponent.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export function AppComponent() {
+  return (
+    <div>123</div>
+  );
+}
